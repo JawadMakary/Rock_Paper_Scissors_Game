94706ebb13866fda7722b40305aa9fadd27c2fdb
jawad <jawadhalbymakary@gmail.com>
2021-08-22 02:09:46 +0300

    contest section is created and hidden

---
+++ js/script.js
@@ -0,0 +1,9 @@
+
+const pickUserHand=(hand)=>{
+    // console.log(hand)
+    let contest=document.querySelector(".contest")
+
+    let hands = document.querySelector(".hands")
+    hands.style.display='none'
+    contest.style.display='flex'
+}
